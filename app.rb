0eca8ecc89c42ebc4399d79b85d7df7ff4469efc
Bably Kumari and Elrey Belmonti <bably@>
2017-09-27 10:52:29 -0700

    create route to add recipe

--- app.rb
+++ app.rb
@@ -13,3 +13,15 @@ get("/") do
   @types = Type.all
   erb(:index)
 end
+
+post("/add_recipe") do
+  type_id = params['type-id']
+  name = params['name']
+  instructions = params['instructions']
+  @new_recipe =Recipe.new({name: name, instructions: instructions, type_id: type_id})
+  if @new_recipe.save
+    erb(:recipe_list)
+  else
+    redirect "/"
+  end
+end
