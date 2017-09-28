52506ec2e7f79e3840828f3a88fa313800a9ef28
Bably Kumari and Elrey Belmonti <bably@>
2017-09-28 13:37:49 -0700

    add rating to rescipies

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -4,44 +4,4 @@ $(document).ready(function(){
     $(".edit-ingredients").removeClass("hide");
   });
 
-  $('.ratings_stars').hover(
-            // Handles the mouseover
-            function() {
-                $(this).prevAll().addBack().addClass('ratings_over');
-                $(this).nextAll().removeClass('ratings_vote');
-            },
-            // Handles the mouseout
-            function() {
-                $(this).prevAll().addBack().removeClass('ratings_over');
-                set_votes($(this).parent());
-            }
-        );
-
-        $('.rate_widget').each(function(i) {
-           var widget = this;
-           var out_data = {
-               widget_id : $(widget).attr('id'),
-               fetch: 1
-           };
-           $.post(
-               '/',
-               out_data,
-               function(INFO) {
-                   $(widget).data( 'fsr', INFO );
-                   set_votes(widget);
-               },
-               'json'
-           );
-       });
 });
-
-function set_votes(widget) {
-
-            var avg = $(widget).data('fsr').whole_avg;
-            var votes = $(widget).data('fsr').number_votes;
-            var exact = $(widget).data('fsr').dec_avg;
-
-            $(widget).find('.star_' + avg).prevAll().andSelf().addClass('ratings_vote');
-            $(widget).find('.star_' + avg).nextAll().removeClass('ratings_vote');
-            $(widget).find('.total_votes').text( votes + ' votes recorded (' + exact + ' rating)' );
-        }
